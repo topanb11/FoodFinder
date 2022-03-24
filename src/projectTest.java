afc2978f376a0dfe2ae617aa1922c578d3466083
Topan Budiman <topanbudiman@Topans-MacBook-Pro.local>
2022-03-24 14:53:24 -0600

    hamper test

---
+++ src/projectTest.java
@@ -0,0 +1,32 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.;
+import static org.junit.Assert.;
+
+public class projectTest {
+
+    public projectTest(){
+
+    }
+
+    @Test
+    public void testHamperConstructor(){
+        Hamper testHamper = new Hamper();
+        assertNotNull("The hamper constructor did not create an object: ", testHamper);
+    }
+
+    @Test
+    public void testHamperAddClientToHamperGoodData(){
+        Hamper testHamper = new Hamper();
+        Client testClient = new Client(1);
+        testHamper.addClientToHamper(testClient.getClientInfo());
+        ArrayList<Client> expectedList = new ArrayList<Client>();
+        expectedList.add(testClient);
+        foundList = testHamper.getClientList();
+        assertEquals("Method addClientToHamper did not return the expected result: ", expectedList, foundList);
+    }
+
+    @Test
+    public void testHamperAddClientToHamperInvalid
+
+}
