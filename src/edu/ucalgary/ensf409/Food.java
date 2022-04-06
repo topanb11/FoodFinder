0c033dfc72c78bf76144aa5b21d04b97761a42f7
Jason Nguyen <92648246+jjason-nn@users.noreply.github.com>
2022-04-05 23:05:07 -0600

    food class done mommy

--- src/edu/ucalgary/ensf409/Food.java
+++ src/edu/ucalgary/ensf409/Food.java
@@ -1,4 +1,21 @@
 package edu.ucalgary.ensf409;
 
 public class Food {
+    private final int id;
+    private final String foodName;
+    private final int grainContent;
+    private final int fvContent;
+    private final int proContent;
+    private final int other;
+    private final int calories;
+
+    public Food(int id, String foodName, int grainC, int fvC, int proC, int other, int calories) {
+        this.id = id;
+        this.foodName = foodName;
+        this.grainContent = grainC;
+        this.fvContent = fvC;
+        this.proContent = proC;
+        this.other = other;
+        this.calories = calories;
+    }
 }
