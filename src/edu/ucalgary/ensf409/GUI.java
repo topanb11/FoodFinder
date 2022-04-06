6b702d714ec2ba4ef36f228ff3b6bf8e73f199af
topanb11 <topanbudiman11@gmail.com>
2022-04-05 23:47:13 -0600

    GUI demo

--- src/edu/ucalgary/ensf409/GUI.java
+++ src/edu/ucalgary/ensf409/GUI.java
@@ -2,9 +2,26 @@ package edu.ucalgary.ensf409;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("cock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
+
+        GUI buttonListener = new GUI();
+        JPanel buttonPanel = new JPanel();
+        JButton button = new JButton("weiner");
+        button.addActionListener(buttonListener);
+        buttonPanel.add(button);
+
+        frame.getContentPane().add(BorderLayout.NORTH, buttonPanel);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "COCKK AND BALLS");
     }
 }
