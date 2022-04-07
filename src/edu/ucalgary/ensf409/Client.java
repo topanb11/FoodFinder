5c1ef57594f331df545a7d9ff6c36454fa89185a
Jason Nguyen <92648246+jjason-nn@users.noreply.github.com>
2022-04-06 23:32:02 -0600

    Add documentation and updated GUI

--- src/edu/ucalgary/ensf409/Client.java
+++ src/edu/ucalgary/ensf409/Client.java
@@ -1,3 +1,10 @@
+/*
+    Group 2 edu.ucalgary.ensf409.Food Bank
+    Members: Topan Budiman, Maxwell Couture, Mark Ngu, Jason Nguyen
+    version: @1.3
+    since: @1.0
+ */
+
 package edu.ucalgary.ensf409;
 
 public class Client {
