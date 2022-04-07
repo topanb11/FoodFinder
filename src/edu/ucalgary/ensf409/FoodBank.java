4b97ce7791fbc93808cbb3f2a631247711662d85
topanb11 <topanbudiman11@gmail.com>
2022-04-06 23:47:00 -0600

    Foodbank 1/2

--- src/edu/ucalgary/ensf409/FoodBank.java
+++ src/edu/ucalgary/ensf409/FoodBank.java
@@ -1,16 +1,22 @@
 package edu.ucalgary.ensf409;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FoodBank {
     private final String DBURL;
     private final String USERNAME;
     private final String PASSWORD;
-    private Connection
+    private Connection dbConnect;
     private ArrayList<Food> foodList = new ArrayList<>();
 
-    public FoodBank() {}
+    public FoodBank(String url, String user, String pw) {
+        this.DBURL = url;
+        this.USERNAME = user;
+        this.PASSWORD = pw;
+    }
 
     public Food getFood(int ID){
         return null;
@@ -21,7 +27,14 @@ public class FoodBank {
     }
 
     public void initializeConnection() {
+        try{
+            dbConnect = DriverManager.getConnection(getDburl(), getUsername(), getPassword());
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
-
+    public String getDburl() {return this.DBURL;}
+    public String getUsername() {return this.USERNAME;}
+    public String getPassword() {return this.PASSWORD;}
 }
