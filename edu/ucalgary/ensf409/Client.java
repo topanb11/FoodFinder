488719f080590ee44182d0443aa958d75ad34854
max584 <maxcout584@gmail.com>
2022-04-13 22:31:51 -0600

    Client Done

--- edu/ucalgary/ensf409/Client.java
+++ edu/ucalgary/ensf409/Client.java
@@ -7,7 +7,8 @@
 
 package edu.ucalgary.ensf409;
 
-public class Client {
+import java.sql.*;
+public class Client extends SQL{
     private ClientType clientType;
     private double WHOLEGRAINS;
     private double FRUITVEGGIES;
@@ -20,40 +21,62 @@ public class Client {
      * @param type is an integer representing what enum to use
      */
     public Client(int type){
-        if(type == 0){
-            this.clientType = ClientType.ADULTMALE;
-            this.WHOLEGRAINS = 0.16;
-            this.FRUITVEGGIES = 0.28;
-            this.PROTEIN = 0.26;
-            this.OTHER = 0.30;
-            this.CALORIES = 2500;
-        }
-        else if(type == 1){
-            this.clientType = ClientType.ADULTFEMALE;
-            this.WHOLEGRAINS = 0.16;
-            this.FRUITVEGGIES = 0.28;
-            this.PROTEIN = 0.26;
-            this.OTHER = 0.30;
-            this.CALORIES = 2000;
-        }
-        else if(type == 2){
-            this.clientType = ClientType.CHILDOVER8;
-            this.WHOLEGRAINS = 0.21;
-            this.FRUITVEGGIES = 0.33;
-            this.PROTEIN = 0.31;
-            this.OTHER = 0.15;
-            this.CALORIES = 2200;
-        }
-        else if(type == 3){
-            this.clientType = ClientType.CHILDUNDER8;
-            this.WHOLEGRAINS = 0.21;
-            this.FRUITVEGGIES = 0.33;
-            this.PROTEIN = 0.31;
-            this.OTHER = 0.15;
-            this.CALORIES = 1400;
-        }
-        else{
-            throw new IllegalArgumentException();
+        try {
+            Statement myStmt = getDbConnect().createStatement();
+            ResultSet clientInfo = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS");
+            if (type == 0) {
+                this.clientType = ClientType.ADULTMALE;
+                while(clientInfo.next()){
+                    if(clientInfo.getString("Client").equals("Adult Male")){
+                        this.WHOLEGRAINS = clientInfo.getInt("WholeGrains")/100;
+                        this.FRUITVEGGIES = clientInfo.getInt("FruitsVeggies")/100;
+                        this.PROTEIN = clientInfo.getInt("Protein")/100;
+                        this.OTHER = clientInfo.getInt("Other")/100;
+                        this.CALORIES = clientInfo.getInt("Calories");
+                        break;
+                    }
+                }
+            } else if (type == 1) {
+                this.clientType = ClientType.ADULTFEMALE;
+                while(clientInfo.next()){
+                    if(clientInfo.getString("Client").equals("Adult Female")){
+                        this.WHOLEGRAINS = clientInfo.getInt("WholeGrains")/100;
+                        this.FRUITVEGGIES = clientInfo.getInt("FruitsVeggies")/100;
+                        this.PROTEIN = clientInfo.getInt("Protein")/100;
+                        this.OTHER = clientInfo.getInt("Other")/100;
+                        this.CALORIES = clientInfo.getInt("Calories");
+                        break;
+                    }
+                }
+            } else if (type == 2) {
+                this.clientType = ClientType.CHILDOVER8;
+                while(clientInfo.next()){
+                    if(clientInfo.getString("Client").equals("Child over 8")){
+                        this.WHOLEGRAINS = clientInfo.getInt("WholeGrains")/100;
+                        this.FRUITVEGGIES = clientInfo.getInt("FruitsVeggies")/100;
+                        this.PROTEIN = clientInfo.getInt("Protein")/100;
+                        this.OTHER = clientInfo.getInt("Other")/100;
+                        this.CALORIES = clientInfo.getInt("Calories");
+                        break;
+                    }
+                }
+            } else if (type == 3) {
+                this.clientType = ClientType.CHILDUNDER8;
+                while(clientInfo.next()){
+                    if(clientInfo.getString("Client").equals("Child under 8")){
+                        this.WHOLEGRAINS = clientInfo.getInt("WholeGrains")/100;
+                        this.FRUITVEGGIES = clientInfo.getInt("FruitsVeggies")/100;
+                        this.PROTEIN = clientInfo.getInt("Protein")/100;
+                        this.OTHER = clientInfo.getInt("Other")/100;
+                        this.CALORIES = clientInfo.getInt("Calories");
+                        break;
+                    }
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
         }
 
     }
